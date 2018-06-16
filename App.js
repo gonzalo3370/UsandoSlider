6fc435e0100d3122a807f5c2017a379af37ba03f
gonzalo3370 <63.wearetheworld@gmail.com>
2018-06-16 02:20:04 -0300

    lo estoy usando como un switch

--- App.js
+++ App.js
@@ -3,21 +3,25 @@ import { StyleSheet, Text, Slider, View } from 'react-native';
 
 export default class App extends React.Component {
 
-state = {slideValue: 0 }
-state = {Texto: "Hola"}
+
+
+state = {Texto: "hola"}
+state2 = {cambio: "chau"}
   render() {
     return (
       <View style={styles.container}>
-        <Text style = {styles.paragraph} >{this.state.Texto} </Text>
+        <Text style = {styles.paragraph}>
+              {this.state.Texto} 
+        </Text>
         <Slider
           style={{width: 300, height: 30, borderRadius: 50}}
           minimumValue={0}
           maximumValue={10}
-          value={this.state.slideValue}
-          onValueChange={(value)=> this.setState({ Texto: "value"}) }
+          value={0}
+          onValueChange={(value)=> this.setState({ Texto: this.state2.cambio}) }
+          // creo que lo estoy usando mal. Esto lo podría hacer con el Switch
           maximumTrackTintColor='red'  
           minimumTrackTintColor='blue'/>
-
       </View>
     );
   }
@@ -36,5 +40,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     color: 'blue',
+  },
+  sliderActivo: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'red',
   },
 });

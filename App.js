4b6c48b49b78ffa9a9d032aaecd571bcd8e1aa3f
gonzalo3370 <63.wearetheworld@gmail.com>
2018-06-16 22:20:18 -0300

    slider que cambia de color

--- App.js
+++ App.js
@@ -3,23 +3,19 @@ import { StyleSheet, Text, Slider, View } from 'react-native';
 
 export default class App extends React.Component {
 
-
-
-state = {Texto: "hola"}
-state2 = {cambio: "chau"}
+state = {numero: 115599}
   render() {
     return (
       <View style={styles.container}>
-        <Text style = {styles.paragraph}>
-              {this.state.Texto} 
-        </Text>
+        <Text style = {{color: this.state.numero}} >
+           {this.state.numero}
+        </Text>        
         <Slider
           style={{width: 300, height: 30, borderRadius: 50}}
-          minimumValue={0}
-          maximumValue={10}
-          value={0}
-          onValueChange={(value)=> this.setState({ Texto: this.state2.cambio}) }
-          // creo que lo estoy usando mal. Esto lo podría hacer con el Switch
+          minimumValue={115599}
+          maximumValue={999999}
+          value={this.state.numero}
+          onValueChange={(value)=> this.setState({ numero: value}) }
           maximumTrackTintColor='red'  
           minimumTrackTintColor='blue'/>
       </View>
@@ -36,16 +32,8 @@ const styles = StyleSheet.create({
   },
   paragraph: {
     margin: 24,
-    fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
     color: 'blue',
-  },
-  sliderActivo: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: 'red',
   },
 });
